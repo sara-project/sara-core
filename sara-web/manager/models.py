4bba65886fea54f650289f8a68d8f9d24e0f91b9
Ícaro Jerry <icaro@novusti.com>
2017-07-12 08:22:49 -0300

    Add area to models

--- sara-web/manager/models.py
+++ sara-web/manager/models.py
@@ -17,6 +17,14 @@ class Requirement(models.Model):
     def __str__(self):
         return self.description
 
+class Area(models.Model):
+    id = models.IntegerField
+    code = models.CharField(max_length=20, blank=False, null=False)
+	description = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
 class RoomtType(models.Model):
     id = models.IntegerField
     name = models.CharField(max_length=50, blank=False, null=False)
@@ -31,6 +39,7 @@ class Room(models.Model):
     description = models.CharField(max_length=100, blank=False, null=False)
 	capacity = models.IntegerField
 	type = models.ForeignKey('RoomtType')
+	area = models.ForeignKey('Area')
 
     def __str__(self):
         return self.description
