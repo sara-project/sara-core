0530d0fafebdc520dfbb6a85fde7c6c2917c33d9
Ícaro Jerry <icarojerry@gmail.com>
2017-07-12 09:10:01 -0300

    Fixing some identation errors

--- sara-web/manager/models.py
+++ sara-web/manager/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 class Institution(models.Model):
     id = models.IntegerField
-	acronym = models.CharField(max_length=20, blank=False, null=False)
+    acronym = models.CharField(max_length=20, blank=False, null=False)
     name = models.CharField(max_length=200, blank=False, null=False)
 
 
     def __str__(self):
         return self.name
-		
+
 class Campus(models.Model):
     id = models.IntegerField
     name = models.CharField(max_length=200, blank=False, null=False)
-	address = models.CharField(max_length=200, blank=False, null=False)
-
+    address = models.CharField(max_length=200, blank=False, null=False)
+    institution = models.ForeignKey('Institution')
 
     def __str__(self):
         return self.name
@@ -38,12 +38,12 @@ class Requirement(models.Model):
 class Area(models.Model):
     id = models.IntegerField
     code = models.CharField(max_length=20, blank=False, null=False)
-	description = models.CharField(max_length=100, blank=False, null=False)
+    description = models.CharField(max_length=100, blank=False, null=False)
 
     def __str__(self):
         return self.name
 
-class RoomtType(models.Model):
+class RoomType(models.Model):
     id = models.IntegerField
     name = models.CharField(max_length=50, blank=False, null=False)
 
@@ -55,9 +55,9 @@ class Room(models.Model):
     id = models.IntegerField
     code = models.CharField(max_length=20, blank=False, null=False)
     description = models.CharField(max_length=100, blank=False, null=False)
-	capacity = models.IntegerField
-	type = models.ForeignKey('RoomtType')
-	area = models.ForeignKey('Area')
+    capacity = models.IntegerField
+    type = models.ForeignKey('RoomType')
+    area = models.ForeignKey('Area')
 
     def __str__(self):
         return self.description
