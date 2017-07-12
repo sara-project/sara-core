fe581a5893bcc13c1127a148dcbbaf13da1d67b1
Ícaro Jerry <icaro@novusti.com>
2017-07-12 08:18:41 -0300

    Add room and room type to models

--- sara-web/manager/models.py
+++ sara-web/manager/models.py
@@ -16,3 +16,21 @@ class Requirement(models.Model):
 
     def __str__(self):
         return self.description
+
+class RoomtType(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=50, blank=False, null=False)
+
+
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    id = models.IntegerField
+    code = models.CharField(max_length=20, blank=False, null=False)
+    description = models.CharField(max_length=100, blank=False, null=False)
+	capacity = models.IntegerField
+	type = models.ForeignKey('RoomtType')
+
+    def __str__(self):
+        return self.description
