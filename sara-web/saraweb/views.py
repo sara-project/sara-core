e3af8bdc633eff7589c87c3634544d063b580b9f
Ícaro Jerry <icaro.jerry@gmail.com>
2017-07-12 23:57:26 -0300

    Moving project to [sara-web](https://github.com/S-A-R-A/sara-web) repository

--- sara-web/saraweb/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
-
-def home(request):
-    html = """
-    <h1>Welcome!</h1>
-    """
-    return HttpResponse(html)
