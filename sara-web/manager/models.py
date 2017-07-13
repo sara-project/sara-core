a533371bb5bfda415908accc1bf3f62125bfb561
Ícaro Jerry <icaro.jerry@gmail.com>
2017-07-12 22:06:16 -0300

    Add sub class Meta on models and fixing some stuff

--- sara-web/manager/models.py
+++ sara-web/manager/models.py
@@ -6,9 +6,12 @@ class Institution(models.Model):
     acronym = models.CharField(max_length=20, blank=False, null=False)
     name = models.CharField(max_length=200, blank=False, null=False)
 
-
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Instituição'
+        verbose_name_plural = 'Instituições'
 
 class Campus(models.Model):
     id = models.IntegerField
@@ -19,22 +22,32 @@ class Campus(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Campus'
+        verbose_name_plural = 'Campi'
+
 class RequirementType(models.Model):
     id = models.IntegerField
     name = models.CharField(max_length=50, blank=False, null=False)
 
-
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Tipo do Requisito'
+        verbose_name_plural = 'Tipos dos Requisitos'
 
 class Requirement(models.Model):
     id = models.IntegerField
     type = models.ForeignKey('RequirementType')
     description = models.CharField(max_length=100, blank=False, null=False)
 
-
     def __str__(self):
         return self.description
+
+    class Meta:
+        verbose_name = 'Requisito'
+        verbose_name_plural = 'Requisitos'
 
 class Area(models.Model):
     id = models.IntegerField
@@ -45,13 +58,20 @@ class Area(models.Model):
     def __str__(self):
         return self.code +' - ' + self.description
 
+    class Meta:
+        verbose_name = 'Área'
+        verbose_name_plural = 'Áreas'
+
 class RoomType(models.Model):
     id = models.IntegerField
     name = models.CharField(max_length=50, blank=False, null=False)
 
-
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Tipo da Sala'
+        verbose_name_plural = 'Tipos das Salas'
 
 class Room(models.Model):
     id = models.IntegerField
@@ -64,7 +84,17 @@ class Room(models.Model):
     def __str__(self):
         return self.description
 
+    class Meta:
+        verbose_name = 'Sala'
+        verbose_name_plural = 'Salas'
+
 class Teacher(models.Model):
     id = models.IntegerField
-    name = models.CharField(max_length=100, blank=false, null=false)
-    
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Professor'
+        verbose_name_plural = 'Professores'
