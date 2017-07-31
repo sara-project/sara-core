f353e263798f8502fdd9f3f77aabafacffafad3d
Ícaro Jerry <icaro.jerry@gmail.com>
2017-07-31 08:17:38 -0300

    Add sara-dashboard and sara-shell projects

---
+++ sara-engine/sara-shell/src/org/sara/shell/Main.java
@@ -0,0 +1,12 @@
+package org.sara.shell;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Core.initialize();   
+        Core.initPlugins();
+    }
+}
