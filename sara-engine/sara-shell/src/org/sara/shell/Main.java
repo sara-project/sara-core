942d80db95a9a003eedb11e20f40d140760e8bb1
Ícaro Jerry <icaro.jerry@gmail.com>
2017-08-01 22:30:06 -0300

    Add method to get processor cores

--- sara-engine/sara-shell/src/org/sara/shell/Main.java
+++ sara-engine/sara-shell/src/org/sara/shell/Main.java
@@ -6,6 +6,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Runtime runTime = Runtime.getRuntime();
+        System.out.println("Processor Cores: " + runTime.availableProcessors());
         Core.initialize();   
         Core.initPlugins();
     }
